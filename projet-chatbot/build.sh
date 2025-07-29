ce1b28fe501cc61d291ff6c2cf2fa15cbe423aa4
oradie mbuyi Mpoyi <oraclemby@gmail.com>
2025-07-29 18:47:58 +0100

    Update build.sh

--- projet-chatbot/build.sh
+++ projet-chatbot/build.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
-# Permet au script de s'arrêter en cas d'erreur
 set -o errexit
 
-# Mettre à jour pip et installer les dépendances
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Créer les tables de la base de données.
-# On utilise 'flask shell' pour exécuter du code Python dans le contexte de l'application.
-# C'est une méthode robuste pour s'assurer que db.create_all() est appelé correctement.
-flask shell <<EOF
-from chatbot_app import create_app, db
-app = create_app()
-with app.app_context():
-    db.create_all()
-exit()
-EOF
